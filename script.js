47e697a645b95101d77e28ad947bc4fd6fcb051f
HPWhitnear <60821155+HPWhitnear@users.noreply.github.com>
2021-02-21 18:24:35 +0000

    increased horizontal movement beyond screen width

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function move_down() {
 	console.log(`moving to ${new_page_num} from ${cur_page_num}`)
 	
 	//move cur page up then move new page left
-	$(`#page_${cur_page_num}`).animate({left: "150vh"}, 500, function() {
+	$(`#page_${cur_page_num}`).animate({left: "200vh"}, 500, function() {
 		$(`#page_${new_page_num}`).animate({bottom: "0"}, 500)
 	});
 	
